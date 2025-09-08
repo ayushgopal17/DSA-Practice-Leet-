7873b99d8e8a3a8dc4791a532368d0eae9207e72
Ayush gopal <ayushgopal17@gmail.com>
2025-09-08 17:56:50 +0530

    Time: 0 ms (100%), Space: 46 MB (48.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,54 +1,36 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-         int start = 0;
-        int end = nums.length - 1;
-        int mid = -1;
-
-        while (start <= end) {
-            mid = start + (end - start) / 2;
-            if (target < nums[mid]) {
-                end = mid - 1;
-            } else if (target > nums[mid]) {
-                start = mid + 1;
-            } else {
-                // Target found at mid
-                int first = mid;
-                int last = mid;
-
-                // Binary search for first occurrence
-                start = 0;
-                end = mid - 1;
-                while (start <= end) {
-                    int mid1 = start + (end - start) / 2;
-                    if (target < nums[mid1]) {
-                        end = mid1 - 1;
-                    } else if (target > nums[mid1]) {
-                        start = mid1 + 1;
-                    } else {
-                        first = mid1;
-                        end = mid1 - 1;
-                    }
-                }
-
-                // Binary search for last occurrence
-                start = mid + 1;
-                end = nums.length - 1;
-                while (start <= end) {
-                    int mid2 = start + (end - start) / 2;
-                    if (target < nums[mid2]) {
-                        end = mid2 - 1;
-                    } else if (target > nums[mid2]) {
-                        start = mid2 + 1;
-                    } else {
-                        last = mid2;
-                        start = mid2 + 1;
-                    }
-                }
-
-                return new int[]{first, last};
-            }
-        }
-
-        return new int[]{-1, -1};
+        int left=findbound(nums,target,true);
+        int right=findbound(nums,target,false);
+        return new int[]{left,right};
     }
-}
+
+    private int findbound(int[] nums,int target,boolean isfirst)
+     {
+
+        int start=0;
+        int end=nums.length-1;
+        int ans=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                ans=mid;
+                if(isfirst){
+                    end=mid-1;
+
+                }
+             else{
+                    start=mid+1;
+                }
+            }
+                else if(nums[mid]<target){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+             return ans;
+        }
+       
+    }
