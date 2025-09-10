bcc381ce5e8af97882be8fd77139cca1fa9882a5
Ayush gopal <ayushgopal17@gmail.com>
2025-09-10 13:07:31 +0530

    Time: 0 ms (100%), Space: 43.4 MB (19.15%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,6 +1,6 @@
 class Solution {
     public int searchInsert(int[] arr, int target) {
-        
+
         int start=0;
         int end=arr.length-1;
 
@@ -9,14 +9,14 @@ class Solution {
             if(target==arr[mid]){
                 return mid;
             }
-            else if(target>arr[mid]){
-                start=mid+1;
-                
+            else if(target<arr[mid]){
+                end=mid-1;
             }
             else{
-                end=mid-1;
+                start=mid+1;
             }
         }
         return start;
+        
     }
 }
