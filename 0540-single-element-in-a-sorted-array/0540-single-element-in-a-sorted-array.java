6b61caf4ac162e6d08bd7f4c39aa3e7ef19001a5
Ayush gopal <ayushgopal17@gmail.com>
2025-09-18 12:12:12 +0530

    Time: 0 ms (100%), Space: 50.4 MB (54.3%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,36 +1,25 @@
 class Solution {
     public int singleNonDuplicate(int[] arr) {
-        
-        //if(arr.length==0) return arr[0];
-        if(arr.length==1) return arr[0];
+        int n=arr.length;
 
-        
-        int start=1;
-        int end=arr.length-2;
+       if(arr.length==1) return arr[0];
+       if(arr[0] !=arr[1]) return arr[0];
+       if(arr[n-1] !=arr[n-2]) return arr[n-1];
+       int start=0;
+       int end=n-2;
+       while(start<=end){
+        int mid=start+(end-start)/2;
+        if(arr[mid] !=arr[mid+1] && arr[mid] !=arr[mid-1])
+        return arr[mid];
 
-
-    if(arr[0] !=arr[1] ){
-                return arr[0];
-            }
-
-           if(arr[arr.length-1] !=arr[arr.length-2]){
-            return arr[arr.length-1];
-          }
-        while(start<=end){
-            int mid=start+(end-start)/2;
-
-            
-
-            if(arr[mid] !=arr[mid+1] && arr[mid] !=arr[mid-1])
-            return arr[mid];
-
-            if( (mid%2==1 &&arr[mid-1]==arr[mid]) || ( mid%2==0 &&arr[mid]==arr[mid+1] )){
-              start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
+        if(mid%2==1 && arr[mid]==arr[mid-1] || mid%2==0 && arr[mid]==arr[mid+1]){
+            start=mid+1;
         }
-        return -1;
+        else{
+            end=mid-1;
+        }
+       }
+       return -1;
+       }
+       
     }
-}
