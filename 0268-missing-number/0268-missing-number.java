92443bf11c0f14010bbdb65293dc08578fb42366
Ayush gopal <ayushgopal17@gmail.com>
2025-09-27 15:58:30 +0530

    Time: 1 ms (32.61%), Space: 45.6 MB (21.95%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,19 +1,26 @@
 class Solution {
     public int missingNumber(int[] arr) {
-
-        int n=arr.length;
-       int sum=n*(n+1)/2;
-
-int sum1 =0;
-for(int i=0;i<arr.length;i++){
-    sum1 = sum1+ arr[i];
-
-}
-
-int ans=sum-sum1;
-
-return ans;
-}
-
-
+        //using cyclic sort
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i];
+            if(arr[i]<arr.length && arr[i] !=arr[correct]){
+                swap(arr,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int index=0;index<arr.length;index++){
+            if(arr[index] !=index){
+                return index;
+            }
+        }
+        return arr.length;
     }
+   static void swap(int[] arr, int start,int end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+    }
+}
