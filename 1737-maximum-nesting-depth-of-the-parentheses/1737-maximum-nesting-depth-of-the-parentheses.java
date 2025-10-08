59575eebfd159bbdd2e108aa3b60f9e12fd87c6d
Ayush gopal <ayushgopal17@gmail.com>
2025-10-08 13:48:07 +0530

    Time: 0 ms (100%), Space: 41.5 MB (83.2%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
@@ -1,18 +1,20 @@
 class Solution {
     public int maxDepth(String s) {
 
-        int count=0;
+        int count =0;
         int maxcount=0;
+        
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
-                count++;
-               maxcount=Math.max(count,maxcount);
+            count++;
+            maxcount=Math.max(count,maxcount);
+        
             }
             else if(s.charAt(i)==')'){
                 count--;
+            
             }
         }
         return maxcount;
-        
     }
 }
