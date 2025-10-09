381eb444f2f256d3d439741347ff7ab424e9283f
Ayush gopal <ayushgopal17@gmail.com>
2025-10-09 14:41:57 +0530

    Time: 6 ms (22.1%), Space: 44 MB (99.91%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,20 @@
 class Solution {
     public int romanToInt(String s) {
+        Map<Character,Integer> map=Map.of('I',1,'V',5,'X',10,'L',50,'C',100,'D',500,'M',1000);
 
-        Map <Character,Integer> map=Map.of('I',1,'V',5,'X',10,'L',50,'C',100,'D',500,'M',1000);
-        
+        int sum=0;
+        int i=0;
 
-int sum=0;
-int i=0;
-
-while(i<s.length()){
-    if(i+1<s.length() && map.get(s.charAt(i))<map.get(s.charAt(i+1))){
-        sum=sum+(map.get(s.charAt(i+1))-map.get(s.charAt(i)));
-        i=i+2;
-    }
-    else{
-        sum=sum+map.get(s.charAt(i));
-        i=i+1;
-    }
-}
-return sum;
-
+        while(i<s.length()){
+            if(i+1<s.length() && map.get(s.charAt(i+1))>map.get(s.charAt(i)) )   {
+             sum= sum+   (map.get(s.charAt(i+1))-map.get(s.charAt(i)));
+             i=i+2;
+            }
+            else{
+                sum=sum+map.get(s.charAt(i));
+                i++;
+            }
+        }
+        return sum;
     }
 }
