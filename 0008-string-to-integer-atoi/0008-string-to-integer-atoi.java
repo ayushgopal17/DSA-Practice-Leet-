31c0e9ebe4f529cddce9d305744cdacc28034ec8
Ayush gopal <ayushgopal17@gmail.com>
2025-10-11 11:10:20 +0530

    Time: 1 ms (100%), Space: 42.5 MB (47.75%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -6,21 +6,23 @@ class Solution {
             return 0;
         }
 
-        int i=0;
         long num=0;
         int sign =1;
-        if(s.charAt(i)=='-' || s.charAt(i)=='+'){
-            sign =(s.charAt(i++)=='-') ? -1:1;
-        }
-        while(i<s.length() && Character.isDigit(s.charAt(i))){
-            num=num*10+(s.charAt(i++)-'0');
-        
-        
-        if(num>Integer.MAX_VALUE){
-            return (sign==1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        }
-        }
-    return (int)(sign*num);
+        int i=0;
 
+        if(s.charAt(i)=='-' || s.charAt(i)=='+'){
+            sign=(s.charAt(i++)== '-') ? -1:1;
+        }
+      
+      while(i<s.length() && Character.isDigit(s.charAt(i))){
+        num=num*10+ (s.charAt(i++)-'0');
+      
+
+
+    if(num>Integer.MAX_VALUE){
+        return (sign==1)?Integer.MAX_VALUE : Integer.MIN_VALUE;
+    }
+      }
+      return (int) (sign*num);
     }
 }
