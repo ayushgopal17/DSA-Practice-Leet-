9f0c96b4bc14958b23ac710286fd1ab5deeceebd
Ayush gopal <ayushgopal17@gmail.com>
2025-10-14 15:59:08 +0530

    Time: 57 ms (69.4%), Space: 42.7 MB (77.16%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -5,19 +5,18 @@ class Solution {
         for(int i=0;i<s.length();i++){
             int[] freq=new int[26];
             for(int j=i;j<n;j++){
-                freq[s.charAt(j)-'a']++;
-                int max=0,min=n;
-                for(int f: freq){
-                    if(f>0){
-                        max=Math.max(max,f);
-                        min=Math.min(min,f);
-                    }
-              
+            freq[s.charAt(j)-'a']++;
+            
+            int max=0,min=n;
+            for(int f:freq){
+                if(f>0){
+                  max=Math.max(max,f);
+                  min=Math.min(min,f);
                 }
-                  ans+= max-min;
             }
-          
+            ans +=max-min;
         }
-          return ans;
+        }
+        return ans;
     }
 }
