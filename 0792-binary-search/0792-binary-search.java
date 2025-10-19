ac5686b369aee65243047ccf03c7b70a7e1a2842
Ayush gopal <ayushgopal17@gmail.com>
2025-10-19 10:01:44 +0530

    Time: 0 ms (100%), Space: 46.2 MB (8.22%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,22 +1,23 @@
 class Solution {
     public int search(int[] arr, int target) {
-        int start=0;
-        int end=arr.length;
 
-        while(start<end){
-            int mid=start+(end-start)/2;
-
-            if(target<arr[mid]){
-                end=mid;
-
-            }
-            else if(target>arr[mid]){
-                start=mid+1;
-            }
-            else{
-                return mid;
-            }
-        }
-        return -1;
+        return binarysearch(arr,target,0,arr.length-1);
     }
-}
+
+    static int binarysearch(int[] arr,int target, int s, int e){
+
+    
+        if(s>e){
+            return -1;
+        }
+        int m=s+ (e-s) / 2;
+        if(arr[m]== target){
+            return m;
+        }
+
+        if(target < arr[m]){
+            return binarysearch(arr,target,s,m-1);
+        }
+        return binarysearch(arr,target,m+1,e);
+        }
+    }
