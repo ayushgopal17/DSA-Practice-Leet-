f01f6d0859d9db27d30d0c5b0716623c34955926
Ayush gopal <ayushgopal17@gmail.com>
2025-11-27 12:09:38 +0530

    Time: 2 ms (52.86%), Space: 44.9 MB (14.73%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        ArrayList<String> ans=new ArrayList<>();
+        ArrayList<String> ans =new ArrayList<>();
         int open=n;
         int close=n;
         String curr="";
@@ -8,16 +8,16 @@ class Solution {
         return ans;
     }
     void param(int open,int close,String curr,ArrayList<String> ans){
- if(open==0 && close==0){
-        ans.add(curr);
-        return;
-       }
+        if(open==0 && close==0){
+            ans.add(curr);
+            return ;
+        }
+
         if(open>0){
             param(open-1,close,curr+"(",ans);
         }
         if(close>open){
             param(open,close-1,curr+")",ans);
         }
-      
     }
 }
