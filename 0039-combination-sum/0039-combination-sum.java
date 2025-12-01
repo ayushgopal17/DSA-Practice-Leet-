782bdaa1d7df646e0c4c88631ddd89a37742af55
Ayush gopal <ayushgopal17@gmail.com>
2025-12-01 19:09:53 +0530

    Time: 2 ms (90.41%), Space: 45.7 MB (29.68%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,20 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
-   List<List<Integer>> list= new ArrayList<>();
-   Arrays.sort(nums);
-   backtrack(list,new ArrayList <>(),nums,target,0);
-   return list;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list= new ArrayList<>();
+         comsum(list,new ArrayList<>(),candidates,target,0);
+        return list;
+       
+    }
+    private void comsum(List<List<Integer>> list,List<Integer> templist,int[] nums,int target,int start){
 
-    }    
-     private void backtrack(List<List<Integer>> list,List<Integer> tempList,int [] nums,int remain,int start){
-        if(remain<0)
-            return ;
-        else if(remain ==0) list.add(new ArrayList<>(tempList));
-
-        else{
-            for(int i=start;i<nums.length;i++){
-            tempList.add(nums[i]);
-            backtrack(list,tempList,nums,remain-nums[i],i);
-            tempList.remove(tempList.size()-1);
-
-            }
-        }
-        }
+if(target<0) return;
+if(target==0) list.add(new ArrayList<>(templist));
+else{
+    for(int i=start;i<nums.length;i++){
+        templist.add(nums[i]);
+        comsum(list,templist,nums,target-nums[i],i);
+        templist.remove(templist.size()-1);
+    }
+}
+    }
 }
