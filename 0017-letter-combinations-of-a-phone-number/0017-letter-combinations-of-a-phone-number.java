5525a3e304b548740fcf28d13fef7a9427905b24
Ayush gopal <ayushgopal17@gmail.com>
2025-12-05 11:08:35 +0530

    Time: 3 ms (34.99%), Space: 49.1 MB (6.71%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,24 +1,22 @@
 class Solution {
-    static String[] map={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" };
-    public List<String> letterCombinations(String digits) {
-        
-        if(digits.isEmpty())
+    static String[] map={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public List<String> letterCombinations(String digit) {
+        if(digit.isEmpty())
         return new ArrayList<>();
-        return helper("",digits); }
-        private List<String> helper(String p,String up){
+        return helper("",digit); }
+        private List<String> helper (String p,String up){
             if(up.isEmpty()){
                 ArrayList<String> list=new ArrayList<>();
                 list.add(p);
                 return list;
             }
-           int digit=up.charAt(0)-'0';
-           String letter=map[digit];
-           List<String> list=new ArrayList<>();
-           for(int i=0;i<letter.length();i++){
-            char ch=letter.charAt(i);
-            list.addAll(helper(p+ch,up.substring(1)));
-           }
-           return list;
-
-        }
+            int digit =up.charAt(0)-'0';
+            String letter=map[digit];
+            List<String> list=new ArrayList<>();
+            for(int i=0;i<letter.length();i++){
+                char ch=letter.charAt(i);
+                list.addAll(helper(p+ch,up.substring(1)));
+            }
+            return list;
     }
+}
