2e7c27ef6a5fb08a8b93d783adb412525e638274
Ayush gopal <ayushgopal17@gmail.com>
2026-02-06 11:22:10 +0530

    Time: 0 ms (100%), Space: 46.8 MB (68.93%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,16 +11,16 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-       ListNode fast=head;
-       ListNode slow=head;
+        ListNode fast=head;
+        ListNode slow=head;
 
-       while(fast!=null && fast.next !=null){
-        slow=slow.next;
-         fast=fast.next.next;
-        if(slow==fast){
-            return true;
+        while(fast !=null && fast.next !=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                return true;
+            }
         }
-       }
-return false;
+        return false;
     }
 }
