f32ed91409d2951826a526ea33690a3dad9b0e1b
Ayush gopal <ayushgopal17@gmail.com>
2026-02-20 11:31:39 +0530

    Time: 0 ms (100%), Space: 42.3 MB (34.18%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+       int[] dp= new int[n+1];
+       Arrays.fill(dp,-1);
+       return recur(n,dp);
+
+    }
+    public int recur(int n,int[] dp){
+        if(n<=2){
+            dp[n]=n;
+            return n;
+        }
+        if(dp[n] !=-1) return dp[n];
+        dp[n]=recur(n-1,dp)+ recur(n-2,dp);
+        return dp[n];
+    }
+    
+}
