def91d5a2b48e1edd0ed6a3ba8c0347e52fff396
Ayush gopal <ayushgopal17@gmail.com>
2026-04-13 14:46:58 +0530

    Time: 1 ms (98.66%), Space: 47.6 MB (13.29%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,18 +1,17 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        
         int tsum=0;
         for(int i=0;i<nums.length;i++){
             tsum +=nums[i];
         }
-
+        
         int lsum=0;
         for(int i=0;i<nums.length;i++){
             if(tsum-lsum-nums[i]==lsum){
                 return i;
             }
-            else {
-           lsum +=nums[i];
+            else{
+                lsum +=nums[i];
             }
         }
         return -1;
