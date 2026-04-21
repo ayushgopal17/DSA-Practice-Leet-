eae787178ddc59a5a77d0e42c073526ba2a08756
Ayush gopal <ayushgopal17@gmail.com>
2026-04-21 15:14:06 +0530

    Time: 24 ms (77.04%), Space: 48.9 MB (50.38%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -6,13 +6,12 @@ class Solution {
         int sum=0;
 
         for(int i=0;i<nums.length;i++){
-            sum=sum+nums[i];
-        
+            sum =sum+nums[i];
 
-        if(map.containsKey(sum-k)){
-            count +=map.get(sum-k);
-        }
-        map.put(sum,map.getOrDefault(sum,0)+1);
+            if(map.containsKey(sum-k)){
+                count +=map.get(sum-k);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
         return count;
     }
