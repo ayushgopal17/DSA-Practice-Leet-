e4ad108812782cb925818adaf81e8fc9cc11300c
Ayush gopal <ayushgopal17@gmail.com>
2026-07-11 13:02:44 +0530

    Time: 12 ms (21.57%), Space: 52.5 MB (6.86%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> set= new HashSet<>();
-
+        Set<String> set= new HashSet<>();
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 char num=board[i][j];
@@ -9,18 +8,18 @@ class Solution {
                 if(num=='.'){
                     continue;
                 }
+                String row=num+" in row "+i;
+                String col=num+" in col "+j;
+                String box=num+" in box "+(i/3)+"-"+(j/3);
 
-                String row= num+ " in row "+ i;
-                String col=num+ " in col "+ j;
-                String box=num+ " in box" +(i/3)+ "-"+ (j/3);
-
-                if(set.contains(row)|| set.contains(col)|| set.contains(box)){
+                if(set.contains(row) ||
+                 set.contains(col)||
+                  set.contains(box)){
                     return false;
-                }
-
-                set.add(row);
-                set.add(col);
-                set.add(box);
+                  }
+                  set.add(row);
+                  set.add(col);
+                 set.add(box);
             }
         }
         return true;
