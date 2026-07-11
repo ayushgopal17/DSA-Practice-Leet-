ec948b53f72a729e006eda908d4020384f8f00ba
Ayush gopal <ayushgopal17@gmail.com>
2026-07-11 13:53:01 +0530

    Time: 2 ms (92.23%), Space: 47.7 MB (75.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,16 @@
 class Solution {
-    public void moveZeroes(int[] arr) {
-       
-       int i=0;
-       for(int j=0;j<arr.length;j++){
-
-         if(arr[j] !=0 ){
-
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
+    public void moveZeroes(int[] nums) {
+        
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j] !=0){
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        while(i<nums.length){
+            nums[i]=0;
             i++;
-         }
-       }
-    
-      }
+        }
     }
+}
