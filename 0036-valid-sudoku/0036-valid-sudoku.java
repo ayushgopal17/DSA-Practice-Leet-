6e2b4683c10b0d7fe2abaa1d06eea0b3ff8eaf26
Ayush gopal <ayushgopal17@gmail.com>
2026-07-12 12:57:08 +0530

    Time: 11 ms (26.69%), Space: 52.5 MB (6.91%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -4,23 +4,22 @@ class Solution {
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 char num=board[i][j];
-
-                if(num=='.'){
-                    continue;
-                }
-                String row=num+"r"+i;
-                String col=num+"c"+j;
-                String box=num+"b"+(i/3)+"-"+(j/3);
-
-                if(set.contains(row) ||
-                 set.contains(col)||
-                  set.contains(box)){
-                    return false;
-                  }
-                  set.add(row);
-                  set.add(col);
-                 set.add(box);
+            
+            if(num=='.'){
+                continue;
             }
+            String row=num+"r"+i;
+            String col=num+"c"+j;
+            String box=num+"b"+(i/3)+"-"+(j/3);
+
+            if(set.contains(row)|| set.contains(col)|| set.contains(box)){
+                return false;
+            }
+
+            set.add(row);
+            set.add(col);
+            set.add(box);
+        }
         }
         return true;
     }
