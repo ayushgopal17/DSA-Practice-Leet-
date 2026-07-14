eb412010d7bf685542b675a9f7fe66e963dd9e11
Ayush gopal <ayushgopal17@gmail.com>
2026-07-14 16:33:23 +0530

    Time: 7 ms (12.54%), Space: 46.3 MB (6.58%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,27 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x==0)
-        {
-            return true;
-        }
-int temp=x;
-int dig=0;
-
-        while(x>0)
-        {
-        int  n=x%10;
-          x=x/10;
-       dig =dig*10+n;
-        }
-        if(temp==dig)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-
         
+        String s= String.valueOf(x);
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)== s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
     }
 }
