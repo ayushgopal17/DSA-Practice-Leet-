c0049ca53547d39f0a2fa6101af9c93516c16776
Ayush gopal <ayushgopal17@gmail.com>
2026-07-15 13:28:30 +0530

    Time: 14 ms (31.43%), Space: 46.3 MB (31.68%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-
-        int i=0;
-        int j=s.length()-1;
-
-        while(i<j){
-            if(s.charAt(i) ==s.charAt(j)){
-                i++;
-                j--;
-            }
-            else{
-                return false;
-            }
+       s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase() ;
+       int i=0;
+       int j=s.length()-1;
+       while(i<j){
+        if(s.charAt(i)== s.charAt(j)){
+            i++;
+            j--;
         }
-        return true;
+        else{
+            return false;
+        }
+       }
+       return true;
     }
 }
