7fb6ae0768b4ae848a397b511a3495753092f09e
Ayush gopal <ayushgopal17@gmail.com>
2026-07-18 11:45:03 +0530

    Time: 862 ms (10.5%), Space: 59.9 MB (18.57%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,8 +5,9 @@ class Solution {
         for(int i=0;i<nums.length-2;i++){
             int j=i+1;
             int k=nums.length-1;
+             while(j<k){
             int sum=nums[i]+nums[j]+nums[k];
-            while(j<k){
+           
             if(sum==0){
                 set.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     j++;
